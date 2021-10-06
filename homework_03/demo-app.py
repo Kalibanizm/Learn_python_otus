a9e260cd7f451ff1b784053ce48246f1c3183e9d
Kalibanizm <85799380+Kalibanizm@users.noreply.github.com>
2021-10-06 21:49:53 +0300

    Update demo-app.py

--- homework_03/demo-app.py
+++ homework_03/demo-app.py
@@ -2,8 +2,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/ping", summary="Get a message json" ,status_code=200)
-def hello(
-    name: str = "pong",
-):
-
-    return {"message": name}
+def hello():
+    return {"message": "pong"}
